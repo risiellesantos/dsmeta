b6bfe84026b331fd29c212ce15e2151ac18955c8
Risielle Santos <risielle@gmail.com>
2022-07-17 22:03:27 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,17 @@
 import Header from "./componentes/Header"
 import SalesCard from "./componentes/SalesCard"
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
           <div className="dsmeta-container">
             <SalesCard />
-
             </div>
         </section>
       </main>
